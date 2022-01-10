43d415e01790600b212a2c592dc370132eefc231
Kyle Zhao <kylezhao101@gmail.com>
2022-01-09 23:42:14 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # QWIOO Discord BOT
 
-A simple music bot written in discord.py using youtube-dl. 
-
-Starting template I used:
-https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
+A simple music bot written in discord.py using youtube-dl and asyncio.
 
 ![https://imgur.com/a/Wxr1wVx](https://i.imgur.com/QQ6W1YI.jpg)
--- main.py
+++ main.py
@@ -1,49 +1,101 @@
+
 import discord
 from discord.ext import commands
 import os
-from discord import Color
-
 import music
 
 cogs = [music]
-client = commands.Bot(command_prefix='??', intents = discord.Intents.all(), activity = discord.Game(name="music I ??help"))
+client = commands.Bot(command_prefix='??',
+                      intents=discord.Intents.all(),
+                      activity=discord.Game(name="music | ??help"))
+
 
 @client.event
 async def on_ready():
-  print("Bot is ready.")
+    print("Bot is ready.")
+
 
 # Custom Help commands ------------------------------------------------------------------
 client.remove_command("help")
 
-@client.group(invoke_without_command=True, aliases=[""])
+
+@client.group(invoke_without_command=True)
 async def help(ctx):
-  em = discord.Embed(title = "Help", description = "Use ??help <command> for extended info", color=0xF8C514)
-  em.add_field(name = "Music", value = "remove,queue,play,pause,resume")
-  await ctx.send(embed = em)
-  
+    em = discord.Embed(title="help",
+                       description="Use ??help <command> for extended info",
+                       color=0xF8C514)
+    em.add_field(name="Music",
+                 value="volume,remove,queue,nowplaying,play,skip,clear")
+
+    await ctx.send(embed=em)
+
+
 @help.command()
 async def remove(ctx):
-  em = discord.Embed(title = "remove", description = "removes a song from queue", color=0xF8C514)
-  em.add_field(name = "**Syntax**", value = "??remove [position], with position starting from 0")
-  await ctx.send(embed = em)
+    em = discord.Embed(title="remove",
+                       description="removes a song from queue",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??remove [position], with position starting from 1")
+    await ctx.send(embed=em)
+
 
 @help.command()
 async def play(ctx):
-  em = discord.Embed(title = "play", description = "starts the queue", color=0xF8C514)
-  em.add_field(name = "**Syntax**", value = "??play")
-  await ctx.send(embed = em)
+    em = discord.Embed(title="play", description="plays song", color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??play [link or words]")
+    await ctx.send(embed=em)
+
 
 @help.command()
 async def queue(ctx):
-  em = discord.Embed(title = "queue", description = "Adds this song to queue", color=0xF8C514)
-  em.add_field(name = "**Syntax**", value = "??queue [link]")
-  await ctx.send(embed = em)
+    em = discord.Embed(
+        title="queue",
+        description="Displays the current song, as well as queue",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??queue")
+    await ctx.send(embed=em)
 
+
+@help.command()
+async def nowplaying(ctx):
+    em = discord.Embed(
+        title="now playing",
+        description="Displays the current song",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??queue")
+    await ctx.send(embed=em)
+
+
+@help.command()
+async def clear(ctx):
+    em = discord.Embed(
+        title="clear",
+        description="Resets the entire queue",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??clear")
+    await ctx.send(embed=em)    
+
+@help.command()
+async def skip(ctx):
+    em = discord.Embed(
+        title="skip",
+        description="skips the current song",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??skip")
+    await ctx.send(embed=em)
+
+@help.command()
+async def volume(ctx):
+    em = discord.Embed(
+        title="volume",
+        description="Displays the current volume, or sets the volume",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??volume [(value 0 - 100)]")
+    await ctx.send(embed=em)
 #-----------------------------------------------------------------------------------------
 for i in range(len(cogs)):
-  cogs[i].setup(client)
+    cogs[i].setup(client)
 
 my_secret = os.environ['qwiootoken']
 client.run(my_secret)
-
-
