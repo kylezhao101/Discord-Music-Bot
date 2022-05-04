2b9b86ba99068566edf844fcd12646cee4f9a56d
Kyle Zhao <kylezhao101@gmail.com>
2022-05-04 16:11:36 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import discord
 from discord.ext import commands
 import os
@@ -9,23 +8,83 @@ client = commands.Bot(command_prefix='??',
                       intents=discord.Intents.all(),
                       activity=discord.Game(name="music | ??help"))
 
-
 @client.event
 async def on_ready():
     print("Bot is ready.")
+    
 
-# Custom Help commands ------------------------------------------------------------------
+#------------------------------------------------------------------
+
 client.remove_command("help")
-
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
     em = discord.Embed(title="help",
                        description="Use ??help <command> for extended info",
                        color=0xF8C514)
-    em.add_field(name="Music",
-                 value="volume,remove,queue,nowplaying,play,skip,clear")
+    em.add_field(name="Music",                    value="join,volume,remove,queue,nowplaying,play,skip,clear,shuffle")
+  
+    em.add_field(name="**User Favourites**",
+                 value="favourite, favourites, removefavourite, playfavourites")
+    
+    await ctx.send(embed=em)
 
+
+@help.command()
+async def favourite(ctx):
+    em = discord.Embed(title="favourite",
+                       description="adds a song to your favourites list",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??favourite")
+    em.add_field(name="**Aliases**",
+                 value="f, fav")
+    await ctx.send(embed=em)
+
+
+@help.command()
+async def favourites(ctx):
+    em = discord.Embed(title="favourites",
+                       description="shows your favourite songs",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??favourites")
+    em.add_field(name="**Aliases**",
+                 value="fs, favs")
+    await ctx.send(embed=em)  
+  
+@help.command()
+async def removefavourite(ctx):
+    em = discord.Embed(title="removefavourite",
+                       description="removes song from favourites",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??removefavourite [song index starting at 0]")
+    em.add_field(name="**Aliases**",
+                 value="rf, removefav, remfav")
+    await ctx.send(embed=em) 
+  
+@help.command()
+async def playfavourites(ctx):
+    em = discord.Embed(title="playfavourites",
+                       description="plays songs from favourites list \n e.g. ??pf all, ??pf 2, ??pf 0, 2",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??playfavourites [range], range could be <all> to play every song, or a <specific number> for a certain song on your list, or a range of songs specified by <numOne, numTwo>")
+    
+    em.add_field(name="**Aliases**",
+                 value="pf, playfav")
+    await ctx.send(embed=em)
+
+@help.command()
+async def join(ctx):
+    em = discord.Embed(title="join",
+                       description="joins your vc",
+                       color=0xF8C514)
+    em.add_field(name="**Syntax**",
+                 value="??join")
+    em.add_field(name="**Aliases**",
+                 value="j, connect")
     await ctx.send(embed=em)
 
 
@@ -36,13 +95,17 @@ async def remove(ctx):
                        color=0xF8C514)
     em.add_field(name="**Syntax**",
                  value="??remove [position], with position starting from 1")
+    em.add_field(name="**Aliases**",
+                 value="r, rem, rm")
     await ctx.send(embed=em)
 
 
 @help.command()
 async def play(ctx):
-    em = discord.Embed(title="play", description="plays song", color=0xF8C514)
+    em = discord.Embed(title="play", description="plays song, you can search specific songs, or link a playlist", color=0xF8C514)
     em.add_field(name="**Syntax**", value="??play [link or words]")
+    em.add_field(name="**Aliases**",
+                 value="p, sing")
     await ctx.send(embed=em)
 
 
@@ -53,6 +116,8 @@ async def queue(ctx):
         description="Displays the current song, as well as queue",
         color=0xF8C514)
     em.add_field(name="**Syntax**", value="??queue")
+    em.add_field(name="**Aliases**",
+                 value="q, playlist, que")
     await ctx.send(embed=em)
 
 
@@ -62,7 +127,9 @@ async def nowplaying(ctx):
         title="now playing",
         description="Displays the current song",
         color=0xF8C514)
-    em.add_field(name="**Syntax**", value="??queue")
+    em.add_field(name="**Syntax**", value="??np")
+    em.add_field(name="**Aliases**",
+                 value="song, current, currentsong, playing")
     await ctx.send(embed=em)
 
 
@@ -73,6 +140,8 @@ async def clear(ctx):
         description="Resets the entire queue",
         color=0xF8C514)
     em.add_field(name="**Syntax**", value="??clear")
+    em.add_field(name="**Aliases**",
+                 value="clr, cl, cr")
     await ctx.send(embed=em)    
 
 @help.command()
@@ -82,6 +151,8 @@ async def skip(ctx):
         description="skips the current song",
         color=0xF8C514)
     em.add_field(name="**Syntax**", value="??skip")
+    em.add_field(name="**Aliases**",
+                 value="s, next")
     await ctx.send(embed=em)
 
 @help.command()
@@ -91,8 +162,23 @@ async def volume(ctx):
         description="Displays the current volume, or sets the volume",
         color=0xF8C514)
     em.add_field(name="**Syntax**", value="??volume [(value 0 - 100)]")
+    em.add_field(name="**Aliases**",
+                 value="v, vol")
     await ctx.send(embed=em)
+
+@help.command()
+async def shuffle(ctx):
+    em = discord.Embed(
+        title="shuffle",
+        description="Randomizes the queue",
+        color=0xF8C514)
+    em.add_field(name="**Syntax**", value="??shuffle")
+    em.add_field(name="**Aliases**",
+                 value="sf")
+    await ctx.send(embed=em)
+
 #-----------------------------------------------------------------------------------------
+
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
--- music.py
+++ music.py
@@ -1,3 +1,7 @@
+import pymongo
+from pymongo import MongoClient
+import os
+
 import discord
 from discord.ext import commands
 
@@ -8,12 +12,35 @@ import sys
 import traceback
 from async_timeout import timeout
 from functools import partial
-import youtube_dl
+from collections import deque
 from youtube_dl import YoutubeDL
+import youtube_dl
+import datetime
+
+cluster = MongoClient(os.environ['mongoURL'])
+db = cluster["qwiooData"]
+collection = db["userData"]
+
+
+# spotiy modules
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import psutil
+import platform
+
+# ??
+from progress.bar import Bar
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
 
+
+class VoiceError(Exception):
+    pass
+
+client = commands.Bot(command_prefix='??',
+                      intents=discord.Intents.all(),
+                      activity=discord.Game(name="music | ??help"))
 ytdlopts = {
     'format': 'bestaudio/best',
     'extractaudio': True,
@@ -36,6 +63,9 @@ ffmpegopts = {
 }
 
 ytdl = YoutubeDL(ytdlopts)
+
+class YTDLError(Exception):
+  pass
 
 class VoiceConnectionError(commands.CommandError):
     """Custom Exception class for connection errors."""
@@ -69,7 +99,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
             # take first item from a playlist
             data = data['entries'][0]
 
-        embed = discord.Embed(title="", description=f"Queued [{data['title']}]({data['webpage_url']}) [{ctx.author.mention}]", color=0xF8C514)
+        embed = discord.Embed(title="", description=f"Queued [{data['title']}]({data['webpage_url']}) {datetime.timedelta(seconds=(data['duration']))} [{ctx.author.mention}]", color=0xF8C514)
         
         embed.set_thumbnail(url=data['thumbnail'])
         await ctx.send(embed=embed)
@@ -77,7 +107,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         if download:
             source = ytdl.prepare_filename(data)
         else:
-            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
+            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title'], 'duration': str(datetime.timedelta(seconds=(data['duration'])))}
 
         return cls(discord.FFmpegPCMAudio(source), data=data, requester=ctx.author)
 
@@ -110,6 +140,18 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         return ', '.join(duration)
 
+class Spotify(commands.Cog):
+    def getTrackID(self, track):
+        track = sp.track(track)
+        return track["id"]
+    def getPlaylistTrackIDs(self, playlist_id):
+        ids = []
+        playlist = sp.playlist(playlist_id)
+        for item in playlist['tracks']['items']:
+            track = item['track']
+            ids.append(track['id'])
+        return ids
+  
 
 class MusicPlayer(commands.Cog):
     __slots__ = ('bot', '_guild', '_channel', 'voice_client', '_cog', 'queue', 'next', 'current', 'np', 'volume')
@@ -122,32 +164,45 @@ class MusicPlayer(commands.Cog):
         self.voice_client = ctx.voice_client  
 
         self._cog = ctx.cog
-
+        
         self.queue = asyncio.Queue()
         self.next = asyncio.Event()
-
+        self._loop = False
         self.np = None  # Now playing message
         self.volume = .69
         self.current = None
-
+        
         ctx.bot.loop.create_task(self.player_loop())
 
+    @property
+    def loop(self):
+        return self._loop
+
+    @loop.setter
+    def loop(self, value: bool):
+        self._loop = value
+
     async def player_loop(self):
+
         """Our main player loop."""
         await self.bot.wait_until_ready()
 
         while not self.bot.is_closed():
             self.next.clear()
-
-            try:
-                # Wait for the next song. If we timeout - cancel the player and disconnect
-                async with timeout(300): 
-                    source = await self.queue.get()
-            except asyncio.TimeoutError:
-                return self.destroy(self._guild)
+            if not self._loop:
+              try:
+                    # Wait for the next song. If we timeout - cancel the player and disconnect
+                    async with timeout(300): 
+                        source = await self.queue.get()
+              except asyncio.TimeoutError:
+                    return self.destroy(self._guild)
+            
+            if self.loop:
+                  self.queue.join(source)
 
             if not isinstance(source, YTDLSource):
                 # regather to prevent stream expiration
+                
                 try:
                     source = await YTDLSource.regather_stream(source, loop=self.bot.loop)
                 except Exception as e:
@@ -157,29 +212,27 @@ class MusicPlayer(commands.Cog):
 
             source.volume = self.volume
             self.current = source
-
+       
+       
             self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
-            #await ctx.invoke(self.bot.get_command('nowplaying', query=""))
-
-            
-            #await self.bot.invoke(self.bot.now_playing_)
-
-
+          
+                
             embed = discord.Embed(title="", description=f"[{source.title}]({source.web_url}) [{source.requester.mention}] \n`{source.duration}`", color=0xF8C514)
 
             embed.set_author(icon_url=self.bot.user.avatar_url, name=f"Now Playing 🎶")
 
-            embed.set_image(url=source.thumbnail)
+            embed.set_thumbnail(url=source.thumbnail)
             await self._channel.send(embed=embed)
 
             await self.next.wait()
             source.cleanup()
+            
             self.current = None
-           
+
+     
     def destroy(self, guild):
         """Disconnect and cleanup the player."""
         return self.bot.loop.create_task(self._cog.cleanup(guild))
-
 
 class Music(commands.Cog):
     """Music related commands."""
@@ -189,7 +242,7 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.players = {}
-
+        
     async def cleanup(self, guild):
         try:
             await guild.voice_client.disconnect()
@@ -231,16 +284,35 @@ class Music(commands.Cog):
 
         return player
 
+    def _playlist(self, search: str):
+      """Returns a dict with all Playlist entries"""
+      ydl_opts = {
+        'ignoreerrors': True,
+        'quit': True,
+        'source_address': '0.0.0.0'
+      }
+
+      with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+          playlist_dict = ydl.extract_info(search, download=False)
+
+          playlistTitle = playlist_dict['title']
+
+          playlist = dict()
+          for video in playlist_dict['entries']:
+              print()
+
+              if not video:
+                  print('ERROR: Unable to get info. Coninuing...')
+                  continue
+              
+              for prop in ['id', 'title']:
+                  print(prop, '--', video.get(prop))
+                  playlist[video.get('title')] = 'https://www.youtube.com/watch?v=' + video.get('id')
+        
+          return playlist, playlistTitle
+
     @commands.command(name='join', aliases=['connect', 'j'], invoke_without_subcommand=True)
     async def connect_(self, ctx, *, channel: discord.VoiceChannel=None):
-        """Connect to voice.
-        Parameters
-        ------------
-        channel: discord.VoiceChannel [Optional]
-            The channel to connect to. If a channel is not specified, an attempt to join the voice channel you are in
-            will be made.
-        This command also handles moving the bot to different channels.
-        """
         if not channel:
             try:
                 channel = ctx.author.voice.channel
@@ -266,33 +338,74 @@ class Music(commands.Cog):
         if (random.randint(0, 1) == 0):
             await ctx.message.add_reaction('👍')
         await ctx.send(f'**Joined `{channel}`**')
+    
+    @commands.command(name='loop', aliases=['l'])
+    async def _loop(self,ctx):
+      player = self.get_player(ctx)
+      vc = ctx.voice_client
+      if not vc or not vc.is_playing:
+            return await ctx.send('Nothing being played at the moment.')
+      player.loop = not player.loop
+      await ctx.message.add_reaction('✅')
+      await ctx.send("Loop is now: " + str(player.loop))
+
+
+    @commands.command(name='shuffle', aliases=['sf'])
+    async def _shuffle(self, ctx):
+        #player = self.get_player(ctx)
+        player = self.get_player(ctx)
+
+        if len(list(player.queue._queue)) > 1:
+          player.queue._queue = random.sample(list(player.queue._queue), len(list(player.queue._queue)))
+          player.queue._queue = deque(player.queue._queue)
+          print(player.queue._queue)
+          await ctx.send("Shuffled queue")
+          await ctx.invoke(self.queue_info)
+          
+        else:
+          await ctx.send("Empty Queue or queue size is 1.")
 
     @commands.command(name='play', aliases=['sing','p'])
     async def play_(self, ctx, *, search: str):
-        """Request a song and add it to the queue.
-        This command attempts to join a valid voice channel if the bot is not already in one.
-        Uses YTDL to automatically search and retrieve a song.
-        Parameters
-        ------------
-        search: str [Required]
-            The song to search and retrieve using YTDL. This could be a simple search, an ID or URL.
-        """
+        
+        
+        playlist = 0
         await ctx.trigger_typing()
+        player = self.get_player(ctx)
         
         vc = ctx.voice_client
         
         if not vc:
             await ctx.invoke(self.connect_)
 
+        #check if link is a playlist
+        # if search link has spotify in it        
+          
+        if search.__contains__('&list='):
+          playlist = 1
+          await ctx.send('retrieving playlist...')
+          async with ctx.typing():
+            playlist, playlistTitle = self._playlist(search)
+            for _title, _link in playlist.items():
+                try:
+                    source = await YTDLSource.create_source(ctx, _link, loop=self.bot.loop)
+                    await player.queue.put(source)
+
+                except YTDLError as e:
+                    await ctx.send('An error occurred while processing this request: {}'.format(str(e)))
+                    
+            await ctx.send(f'Enqueued `{playlist.__len__()}` songs from **{playlistTitle}**')
+        
+        #continue as usual
         player = self.get_player(ctx)
-
-        source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
-
-        await player.queue.put(source)
+        if playlist == 1:
+          pass
+        elif playlist == 0:
+          source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
+          await player.queue.put(source)
 
     @commands.command(name='pause')
-    async def pause_(self, ctx):
-        """Pause the currently playing song."""
+    async def pause_(self, ctx):  
         vc = ctx.voice_client
 
         if not vc or not vc.is_playing():
@@ -305,8 +418,7 @@ class Music(commands.Cog):
         await ctx.send("Paused ⏸️")
 
     @commands.command(name='resume')
-    async def resume_(self, ctx):
-        """Resume the currently paused song."""
+    async def resume_(self, ctx):       
         vc = ctx.voice_client
 
         if not vc or not vc.is_connected():
@@ -317,12 +429,10 @@ class Music(commands.Cog):
 
         vc.resume()
         await ctx.send("Resuming ⏯️")
-
+      
     @commands.command(name='skip', aliases=['next','s'])
     async def skip_(self, ctx):
-        """Skip the song."""
         vc = ctx.voice_client
-
         if not vc or not vc.is_connected():
             embed = discord.Embed(title="", description="I'm not connected to a voice channel", color=0xF8C514)
             return await ctx.send(embed=embed)
@@ -333,8 +443,122 @@ class Music(commands.Cog):
             return
 
         vc.stop()
-    
-    @commands.command(name='remove', aliases=['rm', 'rem'])
+            
+    #TO DO
+    #better error handling
+    #command to add specific favs to queue
+    #command to clear favourites
+    @commands.command(name = 'favourite', aliases = ['f', 'fav'])
+    async def favourite_(self, ctx, *, search):
+        
+        
+        user = ctx.author
+        myquery = { "_id": user.id}
+            
+        ydl_opts = {
+          'format': 'bestaudio', 'noplaylist':'True'
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+          data = ydl.extract_info(f"ytsearch:{search}", download=False)['entries'][0]
+        print(data)
+        dict = {
+            'title': data['title'],
+            'webpage_url': data['webpage_url'],
+            'duration': str(datetime.timedelta(seconds=(data['duration'])))
+          }    
+        print(dict)
+        try:
+          if (collection.count_documents(myquery) == 0):
+            post = {"_id": user.id, "favourite": [dict]}
+            collection.insert_one(post)
+                    
+          else:
+            collection.update_one({'_id' :user.id}, {"$push" : {'favourite': dict}})
+          
+          await ctx.channel.send("Added "+ data['title'] +  " to favourites")
+        except discord.ext.commands.errors.MissingRequiredArgument:
+          await ctx.channel.send("Include song after command")
+
+  
+    @commands.command(name = 'favourites', aliases = ['fs', 'favs'])
+    async def favourites_(self, ctx):
+        user = ctx.author
+        myquery = { "_id": user.id }
+        x = collection.find_one(myquery)
+
+        try:
+          favs = x['favourite']
+          fmt = '\n'.join(f"`{(favs.index(_))}.` [{_['title']}]({_['webpage_url']}) | ` {_['duration']} `\n" for _ in favs)
+          
+          embed=discord.Embed(title="Favourites", description=fmt, color=0xF8C514)  
+          embed.set_footer(text=f"{ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+          await ctx.send(embed=embed)
+          
+        except TypeError as e:
+          await ctx.send(e)
+          
+    @commands.command(name= 'removefavourite', aliases = ['rf', 'removefav', 'remfav'])   
+    async def removeFavourite_(self,ctx, pos : int=None):
+      """Removes specified song from user's favourites"""
+      user = ctx.author
+      myquery = { "_id": user.id}
+
+      #check if user exists in database
+      if (collection.count_documents(myquery) > 0):
+  
+        x = collection.find_one(myquery)
+        favs = x['favourite']
+
+        try:
+          await ctx.channel.send("removed " + str(favs[pos]['title']))
+          del favs[pos]
+          collection.replace_one({'_id' :user.id}, {'favourite': favs})
+          
+        except IndexError as e:
+          await ctx.channel.send(e)
+          
+      else:
+        await ctx.channel.send("use ??f to favourite songs")
+
+    @commands.command(name='playfavourites', aliases=['pf','playfav'])
+    async def play_favourites_(self,ctx,*, range):
+        user = ctx.author
+        myquery = { "_id": user.id}
+
+      #check if user exists in database
+        if (collection.count_documents(myquery) > 0):
+          try:
+            x = collection.find_one(myquery)
+            favs = x['favourite'] 
+          
+            if range == 'all' or range == 'a':
+              to_play = favs
+            elif ',' in range:
+              start, stop = range.split(',')
+              
+              to_play = favs[int(start) : (int(stop)+1)]
+                            
+            elif len(str(range)) >= 1:
+              
+              to_play = favs[(int(range)) : (int(range)+1)]
+              print(to_play)
+        
+            for song in to_play:
+              print(song)
+              link = song['webpage_url']
+              print(type(link))
+              
+              await ctx.invoke(self.bot.get_command('play'), search = link)
+
+          except ValueError as e:
+            await ctx.channel.send(e)
+          
+        else:
+          await ctx.channel.send("use ??f to favourite songs")
+#--------------------------------------------------------------------------------
+
+  
+    @commands.command(name='remove', aliases=['rm', 'rem', 'r'])
     async def remove_(self, ctx, pos : int=None):
         """Removes specified song from queue"""
 
@@ -385,11 +609,11 @@ class Music(commands.Cog):
             embed = discord.Embed(title="", description="queue is empty", color=0xF8C514)
             return await ctx.send(embed=embed)
 
-       
+        print(player.queue._queue)
         # Grabs the songs in the queue...
         upcoming = list(itertools.islice(player.queue._queue, 0, int(len(player.queue._queue))))
 
-        fmt = '\n'.join(f"`{(upcoming.index(_)) + 1}.` [{_['title']}]({_['webpage_url']}) | ` {vc.source.duration} Requested by: {_['requester']}`\n" for _ in upcoming)
+        fmt = '\n'.join(f"`{(upcoming.index(_)) + 1}.` [{_['title']}]({_['webpage_url']}) | ` {_['duration']} Requested by: {_['requester']}`\n" for _ in upcoming)
         
         fmt = f"\n__Now Playing__:\n[{vc.source.title}]({vc.source.web_url}) | ` {vc.source.duration} Requested by: {vc.source.requester}`\n\n__Up Next:__\n" + fmt + f"\n**{len(upcoming)} songs in queue**"
 
@@ -466,7 +690,8 @@ class Music(commands.Cog):
               elif '🔊' in str(res.emoji):
                       print('<<Displaying volume>>')                      
                       await ctx.invoke(self.change_volume)             
-                      
+
+                    
     @commands.command(name='volume', aliases=['vol', 'v'])
     async def change_volume(self, ctx, *, vol: float=None):
         """Change the player volume.
@@ -515,7 +740,8 @@ class Music(commands.Cog):
         await ctx.send('**Successfully disconnected**')
 
         await self.cleanup(ctx.guild)
+    
 
-
+      
 def setup(bot):
     bot.add_cog(Music(bot))
